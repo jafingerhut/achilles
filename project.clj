5e6256fd0a85def8f547d5eb1aa6c18e3e733e98
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2013-05-10 17:09:20 -0700

    Clean up project.clj strings and comments.  No substantive changes.

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject achilles "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject achilles "0.2.0"
+  :description "Simple-minded parallel performance test for Clojure/JVM"
+  :url "http://github.com/jafingerhut/achilles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [
-                 [org.clojure/clojure "1.5.1"]
-                 ;; [org.reflections/reflections "0.9.9-RC1"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :main achilles.core
   :source-paths [ "src/clojure" ]
   :java-source-paths [ "src/java" ]
